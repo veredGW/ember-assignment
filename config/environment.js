9d3781ccb9b648dad7ee29c3c0c533a8239cf1d2
Vered Weisz Gutfreund <vered@tailorbrands.com>
2021-11-28 10:03:33 +0200

    changing to pods and adding card component

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function (environment) {
   let ENV = {
     modulePrefix: 'ember-assignment',
+    podModulePrefix: 'ember-assignment/pods',
     environment,
     rootURL: '/',
     locationType: 'auto',

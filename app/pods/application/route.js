46e3b6cd434d1c1a16830eb2a3d5786b970f6d3c
Vered Weisz Gutfreund <vered@tailorbrands.com>
2021-11-28 14:27:33 +0200

    adding card component with getter

--- app/pods/application/route.js
+++ app/pods/application/route.js
@@ -1,3 +1,18 @@
 import Route from '@ember/routing/route';
+import fetch from 'fetch';
 
-export default class ApplicationRoute extends Route {}
+export default class ApplicationRoute extends Route {
+  async model() {
+    debugger;
+    let response = await fetch(
+      'https://api.airtable.com/v0/appedbrLc74Dun7pV/Content',
+      {
+        headers: {
+          Authorization: 'Bearer key8MxwcnvllBPBro',
+        },
+      }
+    );
+    let data = await response.json();
+    return data.records;
+  }
+}
---
+++ app/pods/components/card/component.js
@@ -0,0 +1,17 @@
+import Component from '@glimmer/component';
+
+export default class CounterComponent extends Component {
+  get headLine() {
+    return this.args.cardFields.Headline;
+  }
+  get subHeadLine() {
+    return this.args.cardFields["Sub-headline"];
+  }
+  get imageUrl() {
+    // debugger
+    if (this.args.cardFields.HeaderImage === undefined)
+      return ""
+    else
+      return this.args.cardFields.HeaderImage[0].url;
+  }
+}
